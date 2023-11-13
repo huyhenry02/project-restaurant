468644b7e8db2917b7a3559375ef54bc4f48db26
DucHuy <sandbox.duchuy02@gmail.com>
2023-11-13 22:09:12 +0700

    ff

--- resources/views/employee/layout/menubar.blade.php
+++ resources/views/employee/layout/menubar.blade.php
@@ -16,30 +16,6 @@
 
                 <div class="navbar-vertical-content">
                     <ul class="navbar-nav navbar-nav-lg nav-tabs">
-                        <!-- Dashboards -->
-                        <li class="navbar-vertical-aside-has-menu show">
-                            <a class="js-navbar-vertical-aside-menu-link nav-link nav-link-toggle active" href="javascript:;" title="Dashboards">
-                                <i class="tio-home-vs-1-outlined nav-icon"></i>
-                                <span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">Dashboards</span>
-                            </a>
-
-                            <ul class="js-navbar-vertical-aside-submenu nav nav-sub">
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="index.html" title="Default">
-                                        <span class="tio-circle nav-indicator-icon"></span>
-                                        <span class="text-truncate">Default</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link " href="dashboard-alternative.html" title="Alternative">
-                                        <span class="tio-circle nav-indicator-icon"></span>
-                                        <span class="text-truncate">Alternative</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <!-- End Dashboards -->
-
                         <li class="nav-item">
                             <small class="nav-subtitle" title="Pages">Pages</small>
                             <small class="tio-more-horizontal nav-subtitle-replacer"></small>
